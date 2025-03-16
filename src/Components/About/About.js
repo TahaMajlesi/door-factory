a6df3f1b17706f048458ad90e969887d0e46e5a6
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-15 23:51:07 -0700

    Update

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,14 +1,48 @@
+import { Pagination } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import image1 from "../../Images/۲۰۲۳۱۱۲۸_۱۲۱۰۰۳.jpg";
+import image2 from "../../Images/۲۰۲۳۱۱۲۸_۱۲۰۸۲۵.jpg";
+import image3 from "../../Images/۲۰۲۴۱۲۲۴_۱۵۴۲۵۴.jpg";
+import image4 from "../../Images/۲۰۲۳۱۱۲۸_۱۴۲۰۱۸.jpg";
+import "swiper/css";
+import "swiper/css/pagination";
 const About = () => {
   return (
-    <div className="w-10/12 mx-auto flex bg-red-400 font-[BYekan,sans] my-10 flex-col">
-      <p className="text-3xl py-2 w-full flex flex-row-reverse">
-        {" "}
-        شرکت ایران در نمایندگی رسمی شرکت گوگسو ترکیه{" "}
-      </p>{" "}
-      <p className="text-xl w-full flex flex-row-reverse">
-        {" "}
-        شرکت ایران در نمایندگی رسمی شرکت گوگسو ترکیه{" "}
-      </p>{" "}
+    <div className="flex w-10/12 mx-auto my-10">
+      <Swiper
+        modules={[Pagination]}
+        spaceBetween={50}
+        slidesPerView={1}
+        loop={true}
+        className="w-6/12"
+        Pagination
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide className="">
+          <img src={image1} alt="" className="h-full" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image2} alt="" className="h-full" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image3} alt="" className="h-full" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={image4} alt="" className="h-full" />
+        </SwiperSlide>
+        ...
+      </Swiper>
+      <div className="w-6/12 flex bg-red-400 font-[BYekan,sans]  flex-col">
+        <p className="text-3xl py-2 w-full flex flex-row-reverse">
+          {" "}
+          شرکت ایران در نمایندگی رسمی شرکت گوگسو ترکیه{" "}
+        </p>{" "}
+        <p className="text-xl w-full flex flex-row-reverse px-4">
+          {" "}
+          شرکت ایران در نمایندگی رسمی شرکت گوگسو ترکیه{" "}
+        </p>{" "}
+      </div>
     </div>
   );
 };
--- src/Components/Slider/Slider.js
+++ src/Components/Slider/Slider.js
@@ -1,22 +1,26 @@
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
 import "swiper/css";
+import "swiper/css/pagination";
 import image from "../../Images/۲۰۲۳۱۱۲۴_۱۳۵۱۰۹.jpg";
 // Import Swiper styles
 
 const Slider = () => {
   return (
     <Swiper
+      modules={[Pagination]}
       spaceBetween={50}
       slidesPerView={1}
       loop={true}
-      onSwiper={(swiper) => console.log(swiper)}
+      Pagination
+      pagination={{ clickable: true }}
     >
       <SwiperSlide>
         <div className="bg-gray-900 opacity-50 z-40 flex absolute w-full h-full ">
           {" "}
         </div>{" "}
-        <div className="w-full flex absolute z-50 p-10  flex-col">
+        <div className="w-full flex absolute z-50 pt-14 pe-28 flex-col">
           <p className="text-white w-full flex flex-row-reverse text-3xl">
             شرکت ایران در{" "}
           </p>{" "}
@@ -24,14 +28,14 @@ const Slider = () => {
             {" "}
             متن{" "}
           </p>{" "}
-        </div>
+        </div>{" "}
         <img src={image} alt="" className="w-full" />
       </SwiperSlide>{" "}
       <SwiperSlide>
         <div className="bg-gray-900 opacity-50 z-40 flex absolute w-full h-full ">
           {" "}
         </div>{" "}
-        <div className="w-full flex absolute z-50 p-10  flex-col">
+        <div className="w-full flex absolute z-50 pt-14 pe-28  flex-col">
           <p className="text-white w-full flex flex-row-reverse text-3xl">
             شرکت ایران در{" "}
           </p>{" "}
@@ -39,14 +43,14 @@ const Slider = () => {
             {" "}
             متن{" "}
           </p>{" "}
-        </div>
+        </div>{" "}
         <img src={image} alt="" className="w-full" />
       </SwiperSlide>{" "}
       <SwiperSlide>
         <div className="bg-gray-900 opacity-50 z-40 flex absolute w-full h-full ">
           {" "}
         </div>{" "}
-        <div className="w-full flex absolute z-50 p-10  flex-col">
+        <div className="w-full flex absolute z-50 pt-14 pe-28  flex-col">
           <p className="text-white w-full flex flex-row-reverse text-3xl">
             شرکت ایران در{" "}
           </p>{" "}
@@ -54,14 +58,14 @@ const Slider = () => {
             {" "}
             متن{" "}
           </p>{" "}
-        </div>
+        </div>{" "}
         <img src={image} alt="" className="w-full" />
       </SwiperSlide>{" "}
       <SwiperSlide>
         <div className="bg-gray-900 opacity-50 z-40 flex absolute w-full h-full ">
           {" "}
         </div>{" "}
-        <div className="w-full flex absolute z-50 p-10  flex-col">
+        <div className="w-full flex absolute z-50 pt-14 pe-28  flex-col">
           <p className="text-white w-full flex flex-row-reverse text-3xl">
             شرکت ایران در{" "}
           </p>{" "}
@@ -69,7 +73,7 @@ const Slider = () => {
             {" "}
             متن{" "}
           </p>{" "}
-        </div>
+        </div>{" "}
         <img src={image} alt="" className="w-full" />
       </SwiperSlide>{" "}
     </Swiper>
