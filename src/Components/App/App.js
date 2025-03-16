844cb5184546725143995e6c96e1b1acbec92e0f
TahaMajlesi <186780445+TahaMajlesi@users.noreply.github.com>
2025-03-16 01:02:13 -0700

    Update

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,13 @@
 import About from "../About/About";
 import Navbar from "../Navbar/Navbar";
+import Products from "../Products/Products";
 import Slider from "../Slider/Slider";
 
 const App = () => {
   return (
     <>
       {" "}
-      <Navbar /> <Slider /> <About />
+      <Navbar /> <Slider /> <About /> <Products />
     </>
   );
 };
